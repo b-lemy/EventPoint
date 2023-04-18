c770ac1e5c195f0eb7921b9a6f711e8f4d553dad
Scarion <irenescarion88@gmail.com>
2023-04-19 01:26:36 +0300

    Homepage continuation

--- lib/screens/event.dart
+++ lib/screens/event.dart
@@ -1,4 +1,5 @@
-// import 'package:flutter/material.dart';
+import 'package:digital_invitation_card/screens/homepage.dart';
+import 'package:flutter/material.dart';
 
 // class EventScreen extends StatefulWidget {
 //   const EventScreen({super.key});
@@ -32,3 +33,162 @@
 //     );
 //   }
 // }
+
+class EventScreen extends StatefulWidget {
+  const EventScreen({super.key});
+
+  @override
+  State<EventScreen> createState() => _EventScreenState();
+}
+
+class _EventScreenState extends State<EventScreen> {
+  final List<bool> _selections = [true, false];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 250, 8, 0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        suffixIcon: const Icon(Icons.arrow_drop_down),
+                        hintText: "Select Event Type",
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFFFCB549))),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Event Date",
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFFFCB549))),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Event Venue",
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFFFCB549))),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Description",
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFFFCB549))),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/homepage.png'),
+                    fit: BoxFit.cover),
+              ),
+              height: MediaQuery.of(context).size.height * .3,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(12, 42, 6, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Event',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      iconSize: 10.0,
+                      color: Colors.white,
+                      icon: const Icon(Icons.arrow_back_ios_new_outlined),
+                      onPressed: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomepageScreen(
+                                    phoneNumber: AutofillHints.countryCode,
+                                  )),
+                        )
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 160, 8, 8),
+                child: ToggleButtons(
+                  //color: const Color(0xFFF3F3F3),
+                  //splashColor: Colors.amber,
+                  // highlightColor: Colors.amber,
+
+                  borderRadius: BorderRadius.circular(6.0),
+                  selectedColor: Colors.white,
+                  fillColor: const Color(0xFFFCB549),
+                  isSelected: _selections,
+                  onPressed: (index) {
+                    setState(() {
+                      _selections[index] = !_selections[index];
+                      if (index == 0) {
+                        _selections[1] = !_selections[0];
+                      } else {
+                        _selections[0] = !_selections[1];
+                      }
+                    });
+                  },
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 40.0),
+                      child: const Text(
+                        "create event",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16.0),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 40.0),
+                      child: const Text(
+                        "manage events",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/imageslider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class ImageSlider extends StatefulWidget {
+  const ImageSlider({super.key});
+
+  @override
+  State<ImageSlider> createState() => _SliderState();
+}
+
+class _SliderState extends State<ImageSlider> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height * 0.2,
+        width: double.maxFinite,
+        child: CarouselSlider(
+          disableGesture: false,
+          items: [
+            Image.asset(
+              'assets/home3.jpg',
+            ),
+            Image.asset(
+              'assets/home1.jpg',
+              //fit: BoxFit.cover,
+            ),
+            Image.asset(
+              'assets/home2.jpg',
+              //fit: BoxFit.cover,
+            )
+          ],
+          options: CarouselOptions(
+            autoPlay: true,
+            enlargeFactor: 5.0,
+            enlargeStrategy: CenterPageEnlargeStrategy.scale,
+            enlargeCenterPage: true,
+            //aspectRatio: 1.0,
+            onPageChanged: (index, reason) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
