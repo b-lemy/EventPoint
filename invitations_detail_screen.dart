ef5a433a705489738a8a67dd2c11bbc6cb802594
Ahmed Kayungilo <67832553+ahmedkayungilo@users.noreply.github.com>
2023-04-19 12:14:39 +0300

    The one with the card code is the detail screen.
    Nimeweka comment kwenye appropriate class

---
+++ invitations_detail_screen.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+class InvitationsDetailScreen extends StatelessWidget {
+  const InvitationsDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 100,
+          horizontal: 40,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.white,
+                  width: 1.0,
+                ),
+              ),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: ((context) => InvitationsDetailScreen())));
+                },
+                alignment: Alignment.center,
+                icon: const Icon(Icons.chevron_left_outlined),
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: ShapeDecoration(
+                  shape: WeirdBorder(radius: 20), color: Colors.white),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.asset('someimage'),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Center(
+                      heightFactor: 5,
+                      child: GridView(
+                        shrinkWrap: true,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2),
+                        children: [
+                          Container(
+                            child: Column(children: const [
+                              Text('Some little heading'),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text('The Detail',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16))
+                            ]),
+                          ),
+                          Container(
+                            child: Column(children: const [
+                              Text('Some little heading'),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text('The Detail',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16))
+                            ]),
+                          ),
+                          Container(
+                            child: Column(children: const [
+                              Text('Some little heading'),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text('The Detail',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16))
+                            ]),
+                          ),
+                          Container(
+                            child: Column(children: const [
+                              Text('Some little heading'),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text('The Detail',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16))
+                            ]),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// This is the code for the card with corners inverted. For now it does both on top and bottom
+
+class WeirdBorder extends ShapeBorder {
+  final double radius;
+  final double pathWidth;
+
+  WeirdBorder({required this.radius, this.pathWidth = 1});
+
+  @override
+  EdgeInsetsGeometry get dimensions => EdgeInsets.zero;
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+    return Path()
+      ..fillType = PathFillType.nonZero
+      ..addPath(getOuterPath(rect, textDirection: textDirection), Offset.zero);
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) =>
+      _createPath(rect);
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {}
+
+  @override
+  ShapeBorder scale(double t) => WeirdBorder(radius: radius);
+
+  Path _createPath(Rect rect) {
+    final innerRadius = radius + pathWidth;
+    final innerRect = Rect.fromLTRB(rect.left + pathWidth, rect.top + pathWidth,
+        rect.right - pathWidth, rect.bottom - pathWidth);
+
+    final outer = Path.combine(PathOperation.difference, Path()..addRect(rect),
+        _createBevels(rect, radius));
+    final inner = Path.combine(PathOperation.difference,
+        Path()..addRect(innerRect), _createBevels(rect, innerRadius));
+    return Path.combine(PathOperation.union, outer, inner);
+  }
+
+  Path _createBevels(Rect rect, double radius) {
+    return Path()
+      ..addOval(
+          Rect.fromCircle(center: Offset(rect.left, rect.top), radius: radius))
+      ..addOval(Rect.fromCircle(
+          center: Offset(rect.left + rect.width, rect.top), radius: radius))
+      ..addOval(Rect.fromCircle(
+          center: Offset(rect.left, rect.top + rect.height), radius: radius))
+      ..addOval(Rect.fromCircle(
+          center: Offset(rect.left + rect.width, rect.top + rect.height),
+          radius: radius));
+  }
+}
---
+++ invitations_list_screen.dart
@@ -0,0 +1,194 @@
+import 'package:event_point/screens/invitations_detail_screen.dart';
+import 'package:flutter/material.dart';
+
+class InvitationsListScreen extends StatefulWidget {
+  const InvitationsListScreen({super.key});
+
+  @override
+  State<InvitationsListScreen> createState() => _InvitationsListScreenState();
+}
+
+class _InvitationsListScreenState extends State<InvitationsListScreen> {
+  final double edgePadding = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.all(edgePadding),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 1.0,
+                          ),
+                        ),
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: ((context) =>
+                                        InvitationsDetailScreen())));
+                          },
+                          alignment: Alignment.center,
+                          icon: const Icon(Icons.chevron_left_outlined),
+                          color: Colors.white,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: const [
+                          Text(
+                            'INVITATION',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Lorem Ipsum dolor sit amet veni vidi vici.',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.white,
+                  child: ListView(
+                    children: const [
+                      NoInvitationWidget(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NoInvitationWidget extends StatelessWidget {
+  const NoInvitationWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 50,
+        horizontal: 30,
+      ),
+      child: Column(
+        children: [
+          const Text(
+            'You have no invitations!',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Lorem ipsum dolor sit amet.veni vidi vici. Lorem vici dolor vidi amet sit vici. Lorem ipsum dolor sit amet.',
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Image.asset('someimage'),
+        ],
+      ),
+    );
+  }
+}
+
+class InvitationDetailCard extends StatelessWidget {
+  const InvitationDetailCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+      elevation: 7,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CircleAvatar(
+              radius: 35,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'BIRTHDAY PARTY',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet. Veni vidi vici. Lorem veni dolor sit vici.',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.chevron_right_outlined,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
